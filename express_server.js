4028d34fe3c82455753fdc1e30e634c39e0bde35
Sajan Thiara <sajan4@hotmail.com>
2021-03-19 07:42:03 +0000

    finished up analytics with some refactoring on top

--- express_server.js
+++ express_server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cookieSession = require('cookie-session');
+const cookieParser = require('cookie-parser');
 const bodyParser = require("body-parser");
 const methodOverride = require('method-override')
 
@@ -8,20 +9,22 @@ const { urlsForUser } = require('./helpers');
 const { generateRandomString } = require('./helpers');
 
 const bcrypt = require('bcrypt');
-const app = express();
 const PORT = 8080;
+const app = express();
 
+app.use(cookieParser());
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 app.use(cookieSession({
   name: 'session',
   keys: ['key1']
 }));
-app.use(bodyParser.urlencoded({extended: true}));
-// override with POST having ?_method=DELETE
-app.use(methodOverride('_method'));
 
 const urlDatabase = {}; // contains url objects keyed to their shortened URL with the objects containing the associated longURL and userID that created it
 const users = {}; // contains user objects keyed to their randomized ID with those objects contianing their id, email, and password
+
+// also see about making a separate page with a header to handle error messages
 
 app.get('/', (req, res) => {
   for (const userID of Object.keys(users)) {
@@ -34,37 +37,19 @@ app.get('/', (req, res) => {
 
 app.delete("/logout", (req, res) => {
   req.session = null;
-  // res.clearCookie("user_id");
   res.redirect(`/urls`);
 });
 
 app.put("/login", (req, res) => {
-/*   if (getUserByEmail(req.body.email, users)) {
-    return res.send("Error code 400! Email not found in list of registered users!");
-  } */
   const user = getUserByEmail(req.body.email, users);
   if (!user) {
-    // return res.send("Error code 400! Email not found in list of registered users!");
     return res.status(400).send("Email not found in list of registered users!") // use this instead?
   }
   if (bcrypt.compareSync(req.body.password, user.password)) {
-    req.session.user_id = users[user.id].id;      // using req now instead of res?
-    // res.cookie("user_id", users[user].id);
+    req.session.user_id = users[user.id].id;
     return res.redirect(`/urls`);
   }
   return res.status(400).send("Incorrect password!");
-
-  /* for (user in users) {
-    if (req.body.email === users[user].email) {
-      if (bcrypt.compareSync(req.body.password, users[user].password)) {
-        req.session.user_id = users[user].id;      // using req now instead of res?
-        // res.cookie("user_id", users[user].id);
-        return res.redirect(`/urls`);
-      }
-      return res.send("Error code 400! Incorrect password!");
-    }
-  }
-  return res.send("Error code 400! Email not found in list of registered users!"); */
 });
 
 app.post("/register", (req, res) => {
@@ -74,11 +59,6 @@ app.post("/register", (req, res) => {
   if (getUserByEmail(req.body.email, users)) {
     return res.status(400).send("Email already taken!");
   }
-  /*  for (user in users) {
-    if (req.body.email === users[user].email) {
-      return res.send("Error code 400! Email already taken!");  // consider creating a function to do this to DRY code up
-    }
-  } */
   const userID = generateRandomString();
   users[userID] = {
     id: userID,
@@ -86,7 +66,6 @@ app.post("/register", (req, res) => {
     password: bcrypt.hashSync(req.body.password, 10)  // using bcrypt here now
   };
   req.session.user_id = userID;
-  // res.cookie("user_id", userID);
   res.redirect(`/urls`);
 });
 
@@ -98,7 +77,9 @@ app.post("/urls", (req, res) => {
   urlDatabase[randomString] = {
     longURL: req.body.longURL,
     userID: req.session.user_id,
-    visitCount: 0
+    visitCount: 0,
+    uniqueVisitors: [],
+    visitTimes: []
   };
   res.redirect(`/urls/${randomString}`);
 });
@@ -126,9 +107,22 @@ app.put("/urls/:id", (req, res) => {
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  // const longURL = urlDatabase[req.params.shortURL]
+  let uniqueVisitorID;
   const longURL = urlDatabase[req.params.shortURL].longURL;
   urlDatabase[req.params.shortURL].visitCount++;
+  if (req.cookies.uniqueTracker === undefined) {
+    uniqueVisitorID = generateRandomString();
+    res.cookie("uniqueTracker", uniqueVisitorID);
+  } else {
+    uniqueVisitorID = req.cookies.uniqueTracker;
+  };
+  if (!(urlDatabase[req.params.shortURL].uniqueVisitors.includes(uniqueVisitorID))) {
+    urlDatabase[req.params.shortURL].uniqueVisitors.push(uniqueVisitorID); // search URL's array to see if the ID has already been added; if not, add  
+  };
+  urlDatabase[req.params.shortURL].visitTimes.push({
+    visitor_id: uniqueVisitorID,
+    time_of_visit: Date()
+  })
   res.redirect(longURL);
 });
 
@@ -168,15 +162,19 @@ app.get("/urls/:shortURL", (req, res) => {
   if (!(urlDatabase[req.params.shortURL])) {
     return res.status(400).send("URL for given ID does not exist!");
   }
-  if (req.session.user_id !== urlDatabase[req.params.shortURL].userID) {
-    return res.render("urls_show", {shortURL: undefined, longURL: undefined, user_id: undefined}); // used as a placeholder to relay that user is not logged in
-  }
   const templateVars = {
+    correctUser: true,
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL].longURL,
     user_id: users[req.session.user_id],
-    visitCount: urlDatabase[req.params.shortURL].visitCount
+    visitCount: urlDatabase[req.params.shortURL].visitCount,
+    uniqueCount: urlDatabase[req.params.shortURL].uniqueVisitors.length,
+    visitTimes: [ ...urlDatabase[req.params.shortURL].visitTimes ]
   };
+  if (req.session.user_id !== urlDatabase[req.params.shortURL].userID) {
+    templateVars.correctUser = false;
+    return res.render("urls_show", templateVars);
+  }
   res.render("urls_show", templateVars);
 });
 
