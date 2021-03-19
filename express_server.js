778235984ab2ab0861f2d3c867704c360450e103
Sajan Thiara <sajan4@hotmail.com>
2021-03-19 08:53:42 +0000

    added a bunch of comments

--- express_server.js
+++ express_server.js
@@ -12,24 +12,24 @@ const bcrypt = require('bcrypt');
 const PORT = 8080;
 const app = express();
 
-app.use(cookieParser());
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
+app.use(cookieParser());
 app.use(cookieSession({
   name: 'session',
   keys: ['key1']
 }));
 
-const urlDatabase = {}; // contains url objects keyed to their shortened URL with the objects containing the associated longURL and userID that created it
-const users = {}; // contains user objects keyed to their randomized ID with those objects contianing their id, email, and password
+const urlDatabase = {}; // contains url objects keyed to their shortURL; those objects contain the longURL, the userID that created it, a visit count, an array of all unique visitors, and an array of objects that list the visitor and time
+const users = {}; // contains user objects keyed to their randomized ID; those objects contain that id along with an email and password
 
 // also see about making a separate page with a header to handle error messages
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => {  // if a valid user is logged-in, renders the homepage; otherwise, renders the login page
   for (const userID of Object.keys(users)) {
     if (req.session.user_id === userID) {
-      res.redirect(`/urls`);
+      return res.redirect(`/urls`);
     }
   }
   res.redirect(`/login`);
@@ -40,10 +40,10 @@ app.delete("/logout", (req, res) => {
   res.redirect(`/urls`);
 });
 
-app.put("/login", (req, res) => {
+app.put("/login", (req, res) => { // logs a user in if the correct email and password are provided
   const user = getUserByEmail(req.body.email, users);
   if (!user) {
-    return res.status(400).send("Email not found in list of registered users!") // use this instead?
+    return res.status(400).send("Email not found in list of registered users!")
   }
   if (bcrypt.compareSync(req.body.password, user.password)) {
     req.session.user_id = users[user.id].id;
@@ -52,9 +52,9 @@ app.put("/login", (req, res) => {
   return res.status(400).send("Incorrect password!");
 });
 
-app.post("/register", (req, res) => {
+app.post("/register", (req, res) => { // registers a user if they enter a "valid" email (very loose definition for valid) that isn't already taken and a password field that isn't empty
   if (req.body.email === "" || req.body.password === "") {
-    return res.status(400).send("No email or password entered!");  // return added because it was adding the user in anyway (could use an else here? Gary just told us not to)
+    return res.status(400).send("No email or password entered!");
   }
   if (getUserByEmail(req.body.email, users)) {
     return res.status(400).send("Email already taken!");
@@ -63,16 +63,16 @@ app.post("/register", (req, res) => {
   users[userID] = {
     id: userID,
     email: req.body.email,
-    password: bcrypt.hashSync(req.body.password, 10)  // using bcrypt here now
+    password: bcrypt.hashSync(req.body.password, 10)
   };
   req.session.user_id = userID;
   res.redirect(`/urls`);
 });
 
-app.post("/urls", (req, res) => {
+app.post("/urls", (req, res) => { // this generates a shortURL and creates a url object to inject into the urlDatabase
   const randomString = generateRandomString();
   if (req.session.user_id === undefined) {
-    return res.status(400).send("User not logged in, post permission denied!");
+    return res.status(400).send("User not logged in, post permission denied!"); // I think this is to deny curl workarounds...that sounds about right
   }
   urlDatabase[randomString] = {
     longURL: req.body.longURL,
@@ -84,7 +84,7 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${randomString}`);
 });
 
-app.delete("/urls/:shortURL/delete", (req, res) => {
+app.delete("/urls/:shortURL/delete", (req, res) => {  // this deletes URLs from a user's list
   if (req.session.user_id === urlDatabase[req.params.shortURL].userID) {
     delete urlDatabase[req.params.shortURL];
   }
@@ -95,38 +95,36 @@ app.get("/urls/:shortURL/redirect", (req, res) => { // to redirect edit requests
   res.redirect(`/urls/${req.params.shortURL}`);
 });
 
-app.put("/urls/:id", (req, res) => {
+app.put("/urls/:id", (req, res) => {  // this is for editing the longURL associated with a particular shortURL
   if (req.session.user_id === undefined) {
-    return res.status(400).send("User not logged in, post permission denied!");
+    return res.status(400).send("User not logged in, post permission denied!"); // this part is to deny curl workarounds...probably
   }
-  // console.log("which page is this");  // this function is for editing an existing longURL
-  if (req.session.user_id === urlDatabase[req.params.id].userID) {
-    urlDatabase[req.params.id].longURL = req.body.longURL;        // why is this different to every other one? Not quite sure
+  if (req.session.user_id === urlDatabase[req.params.id].userID) {  
+    urlDatabase[req.params.id].longURL = req.body.longURL;
   }
   res.redirect(`/urls`);
 });
 
-app.get("/u/:shortURL", (req, res) => {
+app.get("/u/:shortURL", (req, res) => { // when a Tinyapp url gets visited, a few things happen:
   let uniqueVisitorID;
-  const longURL = urlDatabase[req.params.shortURL].longURL;
-  urlDatabase[req.params.shortURL].visitCount++;
-  if (req.cookies.uniqueTracker === undefined) {
-    uniqueVisitorID = generateRandomString();
+  urlDatabase[req.params.shortURL].visitCount++;  // the raw visit count is incremented
+  if (req.cookies.uniqueTracker === undefined) {  // a unique visitor is defined as any request coming from the same browser; visiting the link while logged in to Tinyapp with the account that created it, or to a different account, or not logged in at all will collectively still count as the same unique visitor if those requests are all made from the same browser
+    uniqueVisitorID = generateRandomString(); // if the url hasn't been visited from a particular browser, a cookie is created and assigned
     res.cookie("uniqueTracker", uniqueVisitorID);
   } else {
     uniqueVisitorID = req.cookies.uniqueTracker;
   };
-  if (!(urlDatabase[req.params.shortURL].uniqueVisitors.includes(uniqueVisitorID))) {
-    urlDatabase[req.params.shortURL].uniqueVisitors.push(uniqueVisitorID); // search URL's array to see if the ID has already been added; if not, add  
+  if (!(urlDatabase[req.params.shortURL].uniqueVisitors.includes(uniqueVisitorID))) { // when a Tinyapp url is used, the stored cookie value needs to be added to that particular URL's array of unique visitor IDs if it hasn't already
+    urlDatabase[req.params.shortURL].uniqueVisitors.push(uniqueVisitorID);
   };
-  urlDatabase[req.params.shortURL].visitTimes.push({
+  urlDatabase[req.params.shortURL].visitTimes.push({  // add details of the visitor and time to the url's array of visits
     visitor_id: uniqueVisitorID,
     time_of_visit: Date()
   })
-  res.redirect(longURL);
+  res.redirect(urlDatabase[req.params.shortURL].longURL); // send the user to the requested website
 });
 
-app.get("/urls", (req, res) => {
+app.get("/urls", (req, res) => {  // displays a list of urls created by a particular user
   const templateVars = {
     urls: { ...urlsForUser(req.session.user_id, urlDatabase) },
     user_id: users[req.session.user_id]
@@ -134,7 +132,7 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-app.get("/urls/new", (req, res) => {
+app.get("/urls/new", (req, res) => {  // allows a logged-in user to access the page where a new shortURL can be created
   if (req.session.user_id === undefined) {
     return res.redirect("/login");
   }
@@ -158,7 +156,7 @@ app.get("/register", (req, res) => {
   res.render("urls_register", templateVars);
 });
 
-app.get("/urls/:shortURL", (req, res) => {
+app.get("/urls/:shortURL", (req, res) => {  // loads the edit page for a shortURL if it exists and if the user that created it is logged in
   if (!(urlDatabase[req.params.shortURL])) {
     return res.status(400).send("URL for given ID does not exist!");
   }
@@ -178,10 +176,10 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-app.get("/urls.json", (req, res) => {
+app.get("/urls.json", (req, res) => { // I'm pretty sure I don't need this
   res.json(urlDatabase);
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`Tinyapp listening on port ${PORT}!`);
 });
